78ef93e5e5b6381cbe36097148c40c55d6ff318c
Oleg Pykhalov <go.wigust@gmail.com>
2022-06-27 12:32:13 +0300

    collector.go: Use lvs

--- collector.go
+++ collector.go
@@ -9,51 +9,39 @@ import (
 )
 
 type lvmCollector struct {
-	vgFreeMetric *prometheus.Desc
 	vgSizeMetric *prometheus.Desc
 }
 
-// LVM Collector contains VG size and VG free in MB
+// LVM Collector contains LV size and VG free in MB
 func newLvmCollector() *lvmCollector {
 	return &lvmCollector{
-		vgFreeMetric: prometheus.NewDesc("lvm_vg_free_bytes",
-			"Shows LVM VG free size in MB",
-			[]string{"vg_name"}, nil,
-		),
-		vgSizeMetric: prometheus.NewDesc("lvm_vg_bytes_total",
-			"Shows LVM VG total size in MB",
-			[]string{"vg_name"}, nil,
+		vgSizeMetric: prometheus.NewDesc("lvm_lv_size",
+			"Shows LVM LV size size in Bytes",
+			[]string{"lv_name"}, nil,
 		),
 	}
 }
 
 func (collector *lvmCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- collector.vgFreeMetric
 	ch <- collector.vgSizeMetric
 }
 
 // LVM Collect, call OS command and set values
 func (collector *lvmCollector) Collect(ch chan<- prometheus.Metric) {
-	out, err := exec.Command("/sbin/vgs", "--units", "B", "--separator", ",", "-o", "vg_name,vg_free,vg_size", "--noheadings").Output()
+	out, err := exec.Command("lvs", "--separator", ",", "--units", "B", "--noheadings", "-o", "lv_name,lv_size").Output()
 	if err != nil {
 		log.Print(err)
 	}
 	lines := strings.Split(string(out),"\n")
 	for _, line := range lines {
 		values := strings.Split(line,",")
-		if len(values)==3 {
-			free_size, err := strconv.ParseFloat(strings.Trim(values[1],"B"), 64)
+		if len(values)==2 {
+			lv_size, err := strconv.ParseFloat(strings.Trim(values[1],"B"), 64)
 			if err!= nil {
 				log.Print(err)
 			} else {
-				total_size, err := strconv.ParseFloat(strings.Trim(values[2],"B"), 64)
-				if err!= nil {
-					log.Print(err)
-				} else {
-					vg_name := strings.Trim(values[0], " ")
-					ch <- prometheus.MustNewConstMetric(collector.vgFreeMetric, prometheus.GaugeValue, free_size, vg_name)
-					ch <- prometheus.MustNewConstMetric(collector.vgSizeMetric, prometheus.GaugeValue, total_size, vg_name)
-				}
+				lv_name := strings.Trim(values[0], " ")
+				ch <- prometheus.MustNewConstMetric(collector.vgSizeMetric, prometheus.GaugeValue, lv_size, lv_name)
 			}
 		}
 	}
