57b7d0a106e8e123648eb8e7a15c50474f4a74b1
Oleg Pykhalov <go.wigust@gmail.com>
2022-06-27 14:12:31 +0300

    collector: Colletct data percent

--- collector.go
+++ collector.go
@@ -3,45 +3,57 @@ package main
 import (
 	"github.com/prometheus/client_golang/prometheus"
 	"log"
-	"strings"
-	"strconv"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 type lvmCollector struct {
-	vgSizeMetric *prometheus.Desc
+	lvSizeMetric        *prometheus.Desc
+	lvDataPercentMetric *prometheus.Desc
 }
 
 // LVM Collector contains LV size and VG free in MB
 func newLvmCollector() *lvmCollector {
 	return &lvmCollector{
-		vgSizeMetric: prometheus.NewDesc("lvm_lv_size",
+		lvSizeMetric: prometheus.NewDesc("lvm_lv_size",
 			"Shows LVM LV size size in Bytes",
+			[]string{"lv_name"}, nil,
+		),
+		lvDataPercentMetric: prometheus.NewDesc("lvm_lv_data_percent",
+			"Shows LVM LV data percent",
 			[]string{"lv_name"}, nil,
 		),
 	}
 }
 
 func (collector *lvmCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- collector.vgSizeMetric
+	ch <- collector.lvSizeMetric
+	ch <- collector.lvDataPercentMetric
 }
 
 // LVM Collect, call OS command and set values
 func (collector *lvmCollector) Collect(ch chan<- prometheus.Metric) {
-	out, err := exec.Command("lvs", "--separator", ",", "--units", "B", "--noheadings", "-o", "lv_name,lv_size").Output()
+	out, err := exec.Command("lvs", "--separator", ",", "--units", "B", "--noheadings", "-o", "lv_name,lv_size,data_percent").Output()
 	if err != nil {
 		log.Print(err)
 	}
-	lines := strings.Split(string(out),"\n")
+	lines := strings.Split(string(out), "\n")
 	for _, line := range lines {
-		values := strings.Split(line,",")
-		if len(values)==2 {
-			lv_size, err := strconv.ParseFloat(strings.Trim(values[1],"B"), 64)
-			if err!= nil {
+		values := strings.Split(line, ",")
+		if len(values) == 3 {
+			lv_size, err := strconv.ParseFloat(strings.Trim(values[1], "B"), 64)
+			if err != nil {
 				log.Print(err)
 			} else {
-				lv_name := strings.Trim(values[0], " ")
-				ch <- prometheus.MustNewConstMetric(collector.vgSizeMetric, prometheus.GaugeValue, lv_size, lv_name)
+				data_percent, err := strconv.ParseFloat(strings.Trim(values[2], "B"), 64)
+				if err != nil {
+					log.Print(err)
+				} else {
+					vg_name := strings.Trim(values[0], " ")
+					ch <- prometheus.MustNewConstMetric(collector.lvSizeMetric, prometheus.GaugeValue, lv_size, vg_name)
+					ch <- prometheus.MustNewConstMetric(collector.lvDataPercentMetric, prometheus.GaugeValue, data_percent, vg_name)
+				}
 			}
 		}
 	}
